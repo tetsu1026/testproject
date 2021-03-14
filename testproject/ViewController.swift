a7cb533c466995f2cb7082f05a3ecc85958ad123
tetsu1026 <tetesu.w1026@gmail.com>
2021-03-14 16:18:33 +0900

    Update ViewController.swift

--- testproject/ViewController.swift
+++ testproject/ViewController.swift
@@ -18,11 +18,10 @@ struct Test: Codable {
     }
 }
 
-
-
 class ViewController: UIViewController {
     
     private let celId = "celId"
+    private var tests = [Test]()
     
     let tableView: UITableView = {
         let tv = UITableView()
@@ -38,7 +37,7 @@ class ViewController: UIViewController {
         
         tableView.delegate = self
         tableView.dataSource = self
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: celId)
+        tableView.register(TestTableViewCell.self, forCellReuseIdentifier: celId)
         
         getTestApi()
     }
@@ -57,6 +56,10 @@ class ViewController: UIViewController {
                 do {
                     //let json = try JSONSerialization.jsonObject(with: data, options: .fragmentsAllowed)
                     let test = try JSONDecoder().decode([Test].self, from: data)
+                    self.tests = test
+                    DispatchQueue.main.async {
+                        self.tableView.reloadData()
+                    }
                     print("json:",test)
                 } catch(let err) {
                     print("情報の取得に失敗しました。:", err)
@@ -71,15 +74,51 @@ class ViewController: UIViewController {
 
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return tests.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: celId, for: indexPath)
-        cell.backgroundColor = .red
+        let cell = tableView.dequeueReusableCell(withIdentifier: celId, for: indexPath) as! TestTableViewCell
+        //cell.backgroundColor = .red
         
+        cell.Test = tests[indexPath.row]
         return cell
     }
     
     
+}
+
+class TestTableViewCell: UITableViewCell {
+    
+    var Test: Test? {
+        didSet {
+            bodyTextLabel.text = Test?.userName.description
+            bodyTextLabel.text = Test?.comment.debugDescription
+        }
+    }
+    
+    let bodyTextLabel: UILabel = {
+        let label = UILabel()
+        label.text = "something in here"
+        label.font = .systemFont(ofSize: 10)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+       
+        addSubview(bodyTextLabel)
+        [
+            
+            bodyTextLabel.centerYAnchor.constraint(equalTo: centerYAnchor),
+            ].forEach{ $0.isActive = true }
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
